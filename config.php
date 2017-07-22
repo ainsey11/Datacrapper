93ed287f6efffcbf90eba6e7d7901e2ce076f6fd
Ainsey11 <robert@ainsey11.com>
2017-07-22 01:11:29 +0100

    added basic config.php in
    Signed-off-by: Ainsey11 <robert@ainsey11.com>

--- config.php
+++ config.php
@@ -1,7 +1,21 @@
-<?php
 /**
  * Created by PhpStorm.
  * User: Robert
  * Date: 22/07/2017
  * Time: 00:55
  */
+
+<?php
+/**********************************************************************
+  *dbHost = Host of the MySQL DataBase Server
+ *dbUser = Username
+ *dbPass = Password
+ *dbName = Name of your DataBase
+ **********************************************************************/
+$dbHost = '172.16.1.200';
+$dbUser = 'datacrapper';
+$dbPass = 'datacrapper';
+$dbName = 'datacrapperdb';
+$dbC = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName)
+or die('Error Connecting to MySQL DataBase');
+?>
