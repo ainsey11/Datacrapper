e4cb4d8018a043698e43f8bacb4355da25b38e20
Ainsey11 <robert@ainsey11.com>
2017-07-22 01:16:28 +0100

    added rest of auth
    Signed-off-by: Ainsey11 <robert@ainsey11.com>

--- logout.php
+++ logout.php
@@ -5,3 +5,7 @@
  * Date: 22/07/2017
  * Time: 00:56
  */
+session_start(); //Start the current session
+session_destroy(); //Destroy it! So we are logged out now
+header("location:login.php?msg=Successfully Logged out"); // Move back to login.php with a logout message
+?>
