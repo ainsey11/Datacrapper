97be6e69d94f5e8496948e0552444107b73be1d5
Robert Ainsworth <robert@ainsey11.com>
2017-07-26 16:20:45 +0100

    wrong dir

---
+++ Web/content/launchworkalerts.php
@@ -0,0 +1,10 @@
+<?php require "login/loginheader.php"; ?>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Datacrapper Control</title>
+    <meta name="viewport" content="width=device-width, initial-scale=10.0">
+    <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="css/main.css" rel="stylesheet" media="screen">
+  </head>
+<body>
