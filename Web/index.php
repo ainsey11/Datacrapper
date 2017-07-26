f35ceed832ab57b45ad7e712f85f0ab8b615983b
Robert Ainsworth <robert@ainsey11.com>
2017-07-26 15:58:15 +0100

    building index.php up with a rough idea of how I want things

--- Web/index.php
+++ Web/index.php
@@ -5,7 +5,7 @@
   <head>
     <meta charset="utf-8">
     <title>Login</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=10.0">
     <!-- Bootstrap -->
     <link href="css/bootstrap.css" rel="stylesheet" media="screen">
     <link href="css/main.css" rel="stylesheet" media="screen">
@@ -13,9 +13,18 @@
   <body>
     <div class="container">
       <div class="form-signin">
-        <div class="alert alert-success">You have been <strong>successfully logged in</strong>.</div>
-        <a href="login/logout.php" class="btn btn-default btn-lg btn-block">Logout</a>
-      </div>
-    </div> <!-- /container -->
+        <div class="alert alert-success">Welcome to the Datacrapper Control Panel</div>
+	<br>
+	</div>
+	<a href="content/launchworkalerts.php" class="btn btn-default btn-lg btn-block">Launch EdiNag Work Alerts</a>
+	<a href="content/launchfblerts.php" class="btn btn-default btn-lg btn-block">Launch EdiNag Facebook Alerts</a>
+	<a href="content/viewservertemps.php" class="btn btn-default btn-lg btn-block">View Matrix Temperatures</a>
+	<a href="content/.php" class="btn btn-default btn-lg btn-block">Enter Matrix Control Panel</a>
+	<a href="content/.php" class="btn btn-default btn-lg btn-block">Enter Datacrapper Control Admin Panel</a>
+	<div class="container">
+		<div class="form-signin">
+		<a href="login/logout.php" class="btn btn-default btn-lg btn-block">Logout</a>
+		</div>
+	</div>
   </body>
 </html>
