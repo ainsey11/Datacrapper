653d275f0b0c79bf0f57ec5996df5ed6267d69c3
Robert Ainsworth <robert@ainsey11.com>
2017-08-03 09:38:21 +0100

    no need for these at the min, restart requires sudo which is a bit dirty

--- Web/mcp/index.php
+++ Web/mcp/index.php
@@ -17,14 +17,6 @@
         <div class="alert alert-success">Welcome to the Matrix Control Panel</div>
 	<br>
 	</div>
-	<a href="/launchworkalerts.php" class="btn btn-default btn-lg btn-block">Restart Apache</a>
-	<a href="content/launchfblerts.php" class="btn btn-default btn-lg btn-block">Restart MySQL</a>
-	<a href="matrixtemps.php" class="btn btn-default btn-lg btn-block">Restart Zoneminder</a>
-	<a href="matrixtemps.php" class="btn btn-default btn-lg btn-block">Restart Plex Media Server</a>
-	<a href="matrixtemps.php" class="btn btn-default btn-lg btn-block">Restart Sonarr</a>
-	<a href="matrixtemps.php" class="btn btn-default btn-lg btn-block">Restart Couchpotato</a>
-	<a href="matrixtemps.php" class="btn btn-default btn-lg btn-block">Restart SabNZBD</a>
-	<a href="content/.php" class="btn btn-default btn-lg btn-block">View Local Matrix Monitoring</a>
 	<a href="matrixtemps.php" class="btn btn-default btn-lg btn-block">View Matrix Temperatures</a>
 	<a href="diskstats.php" class="btn btn-default btn-lg btn-block">View Matrix Disk Space Statistics</a>
 	<a href="http://matrix:8384" class="btn btn-default btn-lg btn-block">View Matrix Syncthing Control Panel</a>
