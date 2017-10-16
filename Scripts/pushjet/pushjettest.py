eb1214eb401a88fd3e875570785cb7dd0a97232c
Robert Ainsworth <robert@ainsey11.com>
2017-10-16 19:35:50 +0100

    sudo rm -rf / --no-preserve-root ..... OH S*** Wrong command! ... jk - have to cast those as a bool as configparser will treat it as a string

--- Scripts/pushjet/pushjettest.py
+++ Scripts/pushjet/pushjettest.py
@@ -9,17 +9,16 @@ FaviconURL = config.get("NotifService","FaviconURL")
 
 my_service = pushjet.Service.create(ServiceName,FaviconURL)
 
-ShowKeys = config.get("Keys","ShowKeys")
+ShowKeys = bool(config.get("Keys","ShowKeys"))
 
 if ShowKeys == True:
-        print('Secret Key: %s' % my_service.secret_key)
-        print('Public Key: %s' % my_service.public_key)
+    print('Secret Key: %s' % my_service.secret_key)
+    print('Public Key: %s' % my_service.public_key)
 
-CreateQR = config.get("QR","CreateQR")
+CreateQR = bool(config.get("QR","CreateQR"))
 
 if CreateQR == True:
-        import qrcode
-        img = qrcode.make(my_service.public_key)
-        QRSaveLocation = config.get("QR","QRSaveLocation")
-        img.save(QRSaveLocation)
-
+    import qrcode
+    img = qrcode.make(my_service.public_key)
+    QRSaveLocation = config.get("QR","QRSaveLocation")
+    img.save(QRSaveLocation)
