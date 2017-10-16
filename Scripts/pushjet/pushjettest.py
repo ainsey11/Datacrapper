b2c53a51314695c83afdb19622fdda31e2d603b2
Robert Ainsworth <robert@ainsey11.com>
2017-10-16 19:47:22 +0100

    adding initial run mechanism as the keys change when you create a new service. Created method to store the secret key in the config.ini after the inital run has happened

--- Scripts/pushjet/pushjettest.py
+++ Scripts/pushjet/pushjettest.py
@@ -4,21 +4,23 @@ from ConfigParser import SafeConfigParser
 config = SafeConfigParser()
 config.read ('config.ini')
 
-ServiceName = config.get("NotifService","ServiceName")
-FaviconURL = config.get("NotifService","FaviconURL")
+InitialRun = bool(config.get("InitialRun","InitialRun"))
 
-my_service = pushjet.Service.create(ServiceName,FaviconURL)
+if InitialRun == True:
+    ServiceName = config.get("NotifService","ServiceName")
+    FaviconURL = config.get("NotifService","FaviconURL")
+    my_service = pushjet.Service.create(ServiceName,FaviconURL)
 
-ShowKeys = bool(config.get("Keys","ShowKeys"))
+    ShowKeys = bool(config.get("InitialRun","ShowKeys"))
 
-if ShowKeys == True:
-    print('Secret Key: %s' % my_service.secret_key)
-    print('Public Key: %s' % my_service.public_key)
+    if ShowKeys == True:
+        print('Secret Key: %s' % my_service.secret_key)
+        print('Public Key: %s' % my_service.public_key)
 
-CreateQR = bool(config.get("QR","CreateQR"))
+    CreateQR = bool(config.get("InitialRun","CreateQR"))
 
-if CreateQR == True:
-    import qrcode
-    img = qrcode.make(my_service.public_key)
-    QRSaveLocation = config.get("QR","QRSaveLocation")
-    img.save(QRSaveLocation)
+    if CreateQR == True:
+        import qrcode
+        img = qrcode.make(my_service.public_key)
+        QRSaveLocation = config.get("InitialRun","QRSaveLocation")
+        img.save(QRSaveLocation)
