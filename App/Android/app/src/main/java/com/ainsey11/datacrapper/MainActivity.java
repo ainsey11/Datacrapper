36987f0cb1c42972510c2df68c20a882bf9d5565
Ainsey11 <robert@ainsey11.com>
2018-02-11 13:22:10 +0000

    Recloned on my pc and there's some random adk stuff it wants to commit...
    gonna let it do it's thing

--- App/Android/app/src/main/java/com/ainsey11/datacrapper/MainActivity.java
+++ App/Android/app/src/main/java/com/ainsey11/datacrapper/MainActivity.java
@@ -65,19 +65,18 @@ public class MainActivity extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-     //   if (id == R.id.action_EnableVPN) {
-      //      return true;
-       // }
-       // if (id == R.id.action_GetDNS) {
-        //    return true;
-       // }
+       if (id == R.id.action_EnableVPN) {
+           Snackbar mySnackbar = Snackbar.make(findViewById(R.id.action_EnableVPN), "lolz", Snackbar.LENGTH_SHORT);
+           mySnackbar.show();
+        }
 
-
+        if (id == R.id.action_GetDNS) {
+           return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 
