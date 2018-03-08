403be4f76e0304f12e908eeeab5d768ff843a7d3
Robert Ainsworth <robert@ainsey11.com>
2018-03-08 01:00:19 +0000

    Adding in Rating and grab for avg dmg

--- Web/Gaming/PUBG/StatsScraper.py
+++ Web/Gaming/PUBG/StatsScraper.py
@@ -28,7 +28,7 @@ current_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
 
 # Settings, change me up yo!
 url = 'https://pubgtracker.com/profile/pc/AnalSod0my/solo?region=agg'
-grab = ['Wins', 'Kills', 'Losses', 'K/D Ratio', 'Rounds Played', 'Win %', 'Time Survived', 'Top 10s']
+grab = ['Wins', 'Kills', 'Losses', 'K/D Ratio', 'Rounds Played', 'Win %', 'Time Survived', 'Top 10s', 'Rating']
 
 # First, we create some options for our driver, and use said options to create the driver as headless.
 options = webdriver.ChromeOptions()
@@ -164,6 +164,21 @@ json_Top_10s = [
                 }
             }
         ]
+ratingstr = str(output['Rating'])
+ratingstrnocomma = ratingstr.replace(',','')
+rating = int(ratingstrnocomma)
+json_Rating = [
+        {
+            "measurement": "Rating",
+            "tags": {
+                "Player": "AnalSod0my"
+                },
+            "time" : current_time,
+            "fields": {
+                "value": rating
+                }
+            }
+        ]
 client = InfluxDBClient('localhost', 8086, 'root', 'root', 'pubg')
 client.write_points(json_Loss_Count)
 client.write_points(json_Kills)
@@ -173,6 +188,7 @@ client.write_points(json_Win_Percent)
 client.write_points(json_Time_Survived)
 client.write_points(json_Wins)
 client.write_points(json_Top_10s)
+client.write_points(json_Rating)
 
 print('\n Losses:', output['Losses'])
 print('\n Kills:', output['Kills'])
@@ -182,3 +198,4 @@ print('\n Win %:', output['Win %'])
 print('\n Time Survived:', output['Time Survived'])
 print('\n Wins:', output['Wins'])
 print('\n Top 10s:', output['Top 10s'])
+print('\n Rating:', rating)
